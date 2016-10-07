571789d47ead8cfc41d2210b86ad0f6a17850a7a
Jedsadadech <eggliketime@hotmail.com>
2016-10-07 23:39:59 +0700

    Add files via upload

---
+++ Ruby1.rb
@@ -0,0 +1,12 @@
+﻿class MyObjectSystem < BasicObject
+DELEGATE = [:puts, :p]
+ 
+def method_missing(name, *args, &block)
+super unless DELEGATE.include? name
+::Kernel.send(name, *args, &block)
+end
+ 
+def respond_to_missing?(name, include_private = false)
+DELEGATE.include?(name) or super
+end
+end
